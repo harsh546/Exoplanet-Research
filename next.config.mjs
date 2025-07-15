998d525b3fd235d87e69b279a36fcc2b1a114871
harsh546 <harsh.verma2492@gmail.com>
2025-07-15 23:56:49 +0530

    [Removed] Github Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // enables static export (required for GitHub Pages)
-  basePath: "/exoplanet-research", // sets the base path for GitHub Pages
   eslint: {
     ignoreDuringBuilds: true,
   },
