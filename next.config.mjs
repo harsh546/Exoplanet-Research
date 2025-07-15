df4e07fe8ab58e494307f7828e9285db0df31d98
harsh546 <harsh.verma2492@gmail.com>
2025-07-15 23:52:45 +0530

    [Updated]

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // enables static export (required for GitHub Pages)
+  basePath: "/exoplanet-research", // sets the base path for GitHub Pages
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +11,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
